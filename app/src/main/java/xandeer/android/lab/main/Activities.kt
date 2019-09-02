32541d614c11bea6c2691cd81455e9763638ddea
Kevin <kkxandeer@gmail.com>
2019-09-02 17:49:47 +0800

    refactor: set the developing activity as the last one

--- app/src/main/java/xandeer/android/lab/main/Activities.kt
+++ app/src/main/java/xandeer/android/lab/main/Activities.kt
@@ -4,8 +4,6 @@ import xandeer.android.lab.pdftopng.PdfToPngActivity
 import xandeer.android.lab.work.WorkActivity
 
 object Activities {
-  private const val DEVELOPING_INDEX = 2
-
   private val models = arrayOf(
     Model("Main", MainActivity::class.java.name),
     Model("Pdf To Png", PdfToPngActivity::class.java.name),
@@ -14,7 +12,7 @@ object Activities {
 
   val size = models.size
 
-  val developing = get(DEVELOPING_INDEX)
+  val developing = get(size - 1)
 
   fun get(index: Int) = try {
     models[index]
