a701fff4b55e01ff5ddc58703e514fd3604f5bf7
Kevin Du <kkxandeer@gmail.com>
2020-07-29 14:18:17 +0800

    build: update firebase

--- app/src/main/java/xandeer/android/lab/App.kt
+++ app/src/main/java/xandeer/android/lab/App.kt
@@ -3,8 +3,6 @@ package xandeer.android.lab
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import com.google.firebase.FirebaseApp
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 import xandeer.android.lab.utils.log.TimberTreeFactory
 
@@ -19,11 +17,11 @@ class App : Application() {
     context = this
 
     Timber.plant(TimberTreeFactory.getTree())
-
-//    FirebaseApp.initializeApp(this)
-//    FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
   }
 
   val defaultSharedPreferences: SharedPreferences
-    get() = getSharedPreferences(DEFAULT_SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+    get() = getSharedPreferences(
+      DEFAULT_SHARED_PREFERENCES_NAME,
+      Context.MODE_PRIVATE
+    )
 }
